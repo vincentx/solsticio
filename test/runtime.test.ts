c46be8b0f33165a4e86e1582da8a03b926dda75b
Vincent Xu <vincent.hao.xu@gmail.com>
2022-07-04 10:44:52 +0800

    tasking for runtime

---
+++ test/runtime.test.ts
@@ -0,0 +1,11 @@
+import {describe, it} from "vitest";
+
+describe("Solstice runtime", () => {
+    it("should list all extension points available")
+    it("should install extension points from plugin")
+
+    it("should return empty extensions for undefined extension points")
+    it("should install extensions from plugin")
+
+    it("should install get installed extensions for extension points")
+})
