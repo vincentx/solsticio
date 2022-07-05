f80954970943af98d818e5371cf5f63ec1c4ae89
Vincent Xu <vincent.hao.xu@gmail.com>
2022-07-06 01:50:12 +0800

    remove proxy

--- src/iframe-sandbox.ts
+++ src/iframe-sandbox.ts
@@ -15,39 +15,6 @@ type SandboxConfiguration = {
     source: (e: MessageEvent) => Window
 }
 
-export class Proxy<Context> {
-    private _target: Window
-    private readonly _queue: Map<string, (value: any) => void> = new Map()
-
-    constructor(receiver: Window, target: Window) {
-        this._target = target
-
-        receiver.addEventListener('message', (e) => {
-            let message = e.data as SandboxResponse
-            if (this._queue.has(message.id)) {
-                let resolve = this._queue.get(message.id)!
-                this._queue.delete(message.id)
-                resolve(message.response)
-            }
-        })
-    }
-
-    fetch(time: number, fallback: Context): Promise<Context> {
-        return this.timeout(new Promise<Context>((resolve) => {
-            let id = uuid()
-            this._queue.set(id, resolve)
-            this._target.postMessage({
-                id: id,
-                request: 'context'
-            }, '*')
-        }), time, fallback)
-    }
-
-    private timeout<Context>(promise: Promise<Context>, time: number, fallback: Context) {
-        return Promise.race([promise, new Promise<Context>((resolve) => setTimeout(resolve, time, fallback))])
-    }
-}
-
 export class Host {
     private _resolvers: Map<string, (value: any) => void> = new Map()
     private _sandboxes: Map<string, any> = new Map()
