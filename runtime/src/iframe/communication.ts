cec965847a2e700fa7d0e303b545280382caacd7
Vincent Xu <vincent.hao.xu@gmail.com>
2022-07-09 21:31:36 +0800

    refactoring remote to separate out window

--- runtime/src/iframe/communication.ts
+++ runtime/src/iframe/communication.ts
@@ -5,12 +5,17 @@ export type Callable = { _solstice_id: string }
 export type CallableRequest = { id: string, type: 'call', callable: string, parameters: any[] }
 export type CallableResponse = { id: string, type: 'response', response: any }
 
-type Receiver = (value: any) => void
+export interface Endpoint {
+    send: (message: Message) => void
+}
+
 type UUID = string
+type Receiver = (value: any) => void
+type Message = { id: string }
 
 export class Remote {
     private readonly _receivers: Map<UUID, Receiver> = new Map()
-    private readonly _local : Local
+    private readonly _local: Local
 
     constructor(local: Local) {
         this._local = local;
@@ -22,34 +27,56 @@ export class Remote {
         this._receivers.delete(response.id)
     }
 
+    receive_(sender: Endpoint, response: CallableResponse) {
+        if (!this._receivers.has(response.id)) throw 'callable not called'
+        this._receivers.get(response.id)!(this.toLocal_(sender, response.response))
+        this._receivers.delete(response.id)
+    }
+
     send(remote: Window, message: (id: string) => any) {
+        return this.send_({
+            send: (message: { id: string }) => remote.postMessage(message, '*')
+        }, message)
+    }
+
+    send_(sender: Endpoint, message: (id: string) => Message): Promise<any> {
         return new Promise<any>((resolve) => {
             let id = uuid()
             this._receivers.set(id, resolve)
-            remote.postMessage(message(id), '*')
+            sender.send(message(id))
         })
     }
 
     toLocal(context: Context, remote: Window): Context {
-        if (context._solstice_id) return this.toLocalFunction(context, remote)
-        if (Array.isArray(context)) return context.map(v => this.toLocal(v, remote))
-        if (typeof context === 'object') {
-            let result: any = {}
-            for (let key of Object.keys(context))
-                result[key] = this.toLocal(context[key], remote)
-            return result
-        }
-        return context
+        return this.toLocal_({
+            send: (message: { id: string }) => remote.postMessage(message, '*')
+        }, context)
     }
 
-    private toLocalFunction(callable: Callable, remote: Window) {
-        let call = this.send.bind(this)
+    toLocal_(sender: Endpoint, object: any): any {
+        if (object._solstice_id) return this.toLocalFunction_(sender, object)
+        if (Array.isArray(object)) return object.map(v => this.toLocal_(sender, v))
+        if (typeof object === 'object') {
+            let result: any = {}
+            for (let key of Object.keys(object))
+                result[key] = this.toLocal_(sender, object[key])
+            return result
+        }
+        return object
+
+    }
+
+    private toLocalFunction_(sender: Endpoint, callable: Callable) {
+        let call = this.send_.bind(this)
         let local = this._local
         return function (): Promise<any> {
             let parameters = local.toRemote([...arguments])
-            return call(remote, (id) => {
-                return {id: id, type: 'call', callable: callable._solstice_id, parameters: parameters}
-            })
+            return call(sender, (id) => ({
+                id: id,
+                type: 'call',
+                callable: callable._solstice_id,
+                parameters: parameters
+            }))
         }
     }
 }
@@ -58,7 +85,7 @@ export class Local {
     private readonly _callables: Map<UUID, Function> = new Map()
     private readonly _uuid: () => UUID;
 
-    constructor(gen: () => UUID = uuid ) {
+    constructor(gen: () => UUID = uuid) {
         this._uuid = gen;
     }
 
--- runtime/test/iframe/remote.test.ts
+++ runtime/test/iframe/remote.test.ts
@@ -1,123 +1,122 @@
 import {beforeEach, describe, expect, it, vi} from 'vitest'
-import {CallableResponse, Local, Remote} from '../../src/iframe/communication'
-import {v4} from "uuid";
+import {CallableResponse, Endpoint, Local, Remote} from '../../src/iframe/communication'
 
-// @vitest-environment jsdom
 describe('iFrame communication: Remote', () => {
-    let _sandbox: HTMLIFrameElement
     let _remote: Remote
-    let _local: Local
+    let _sender: Endpoint
 
     beforeEach(() => {
-        _local = new Local()
-        _remote = new Remote(_local)
-
-        _sandbox = window.document.createElement('iframe')
-        window.document.body.appendChild(_sandbox)
-
-        vi.mock('uuid', () => {
-            return {
-                v4: vi.fn()
-            }
-        })
-
-        vi.mocked(v4).mockReturnValueOnce('message-id')
+        _remote = new Remote(new Local())
+        _sender = {
+            send: vi.fn()
+        }
     })
 
-    describe('communicate with remote', () => {
-        it('should send message to remote', async () => {
-            let message = remoteReceived()
-
-            _remote.send(_sandbox.contentWindow!, requestRemoteCall.bind(this))
-
-            await expect(message).resolves.toEqual({
-                id: 'message-id',
-                type: 'call',
-                callable: 'callable',
-                parameters: []
-            })
-        })
-
-        it('should return received response from remote to sender', async () => {
-            remoteReceived().then(_ => _remote.receive(response()))
-
-            await expect(_remote.send(_sandbox.contentWindow!, requestRemoteCall.bind(this))).resolves.toEqual('received')
-        })
-
-        it('should throw exception if message id does not match', () => {
-            expect(() => _remote.receive(response())).toThrowError('callable not called')
-        })
-
-        it('should not return to sender after response received', async () => {
-            remoteReceived().then(_ => _remote.receive(response()))
-
-            await _remote.send(_sandbox.contentWindow!, requestRemoteCall.bind(this))
-
-            expect(() => _remote.receive(response())).toThrowError('callable not called')
-        })
-    })
-
-    describe('restore context from remote', () => {
-        it('should restore object from remote context', () => {
-            let context = _remote.toLocal({data: 'data'}, _sandbox.contentWindow!)
+    describe('restore remote object to local', () => {
+        it('should convert remote object to local object', () => {
+            let context = _remote.toLocal_(_sender, {data: 'data'})
             expect(context).toEqual({data: 'data'})
         })
 
-        it('should restore nested object from remote context', () => {
-            let context = _remote.toLocal({nested: {data: 'data'}}, _sandbox.contentWindow!)
+        it('should convert nested object from remote to local object', () => {
+            let context = _remote.toLocal_(_sender, {nested: {data: 'data'}})
             expect(context).toEqual({nested: {data: 'data'}})
         })
 
-        it('should restore array from remote context', () => {
-            let context = _remote.toLocal({array: [1, 2, 3]}, _sandbox.contentWindow!)
+        it('should convert array from remote to local array', () => {
+            let context = _remote.toLocal_(_sender, {array: [1, 2, 3]})
             expect(context).toEqual({array: [1, 2, 3]})
         })
 
-        it('should restore function from remote context', async () => {
-            let message = remoteReceived()
-            let context = _remote.toLocal({func: {_solstice_id: 'func'}}, _sandbox.contentWindow!)
+        it('should convert function from remote to local function', () => {
+            let context = _remote.toLocal_(_sender, {func: {_solstice_id: 'func'}})
 
             context.func('parameter')
 
-            await expect(message).resolves.toEqual(requestRemoteCall('message-id', 'func', 'parameter'))
+            let message: any = vi.mocked(_sender.send).mock.lastCall[0]
+            expect(message.type).toEqual('call')
+            expect(message.parameters).toEqual(['parameter'])
         })
 
-        it('should pass callback as parameter to function restored from remote context', async () => {
-            vi.mocked(v4).mockReset()
-            vi.mocked(v4).mockReturnValueOnce('function-id')
-            vi.mocked(v4).mockReturnValueOnce('message-id')
+        it('should convert function nested in object from remote to local function', async () => {
+            let context = _remote.toLocal_(_sender, {nested: {func: {_solstice_id: 'func'}}})
 
-            let message = remoteReceived()
-            let context = _remote.toLocal({func: {_solstice_id: 'func'}}, _sandbox.contentWindow!)
+            context.nested.func('parameter')
 
-            let callback = () => 'callback'
-            context.func(callback)
-
-            await expect(message).resolves.toEqual(requestRemoteCall('message-id', 'func', {_solstice_id: 'function-id'}))
+            let message: any = vi.mocked(_sender.send).mock.lastCall[0]
+            expect(message.type).toEqual('call')
+            expect(message.parameters).toEqual(['parameter'])
         })
 
+        it('should convert local function parameter to remote', () => {
+            let context = _remote.toLocal_(_sender, {func: {_solstice_id: 'func'}})
 
-        it('should restore function nested in other object from remote context', async () => {
-            let message = remoteReceived()
-            let context = _remote.toLocal({nested: {func: {_solstice_id: 'func'}}}, _sandbox.contentWindow!)
+            context.func(() => 'parameter')
 
-            context.nested.func()
-
-            await expect(message).resolves.toEqual(requestRemoteCall('message-id', 'func'))
+            let message: any = vi.mocked(_sender.send).mock.lastCall[0]
+            expect(message.type).toEqual('call')
+            expect(message.parameters.length).toEqual(1)
+            expect(message.parameters[0]._solstice_id).not.toBe(undefined)
         })
     })
 
-    function requestRemoteCall(id: string, callable: string = 'callable', ...parameters: any[]) {
-        return {id: id, type: 'call', callable: callable, parameters: parameters}
-    }
+    describe('communicate with remote', () => {
+        it('should send message to remote by sender', () => {
+            let message: any
+
+            _remote.send_(_sender, (id) => {
+                message = {id: id, type: 'call', callable: 'callable', parameters: []}
+                return message
+            })
+
+            expect(_sender.send).toHaveBeenCalledWith(message)
+        })
+
+        it('should return remote response to saved promise', async () => {
+            let message: any
+
+            let result = _remote.send_(_sender, (id) => {
+                message = {id: id, type: 'call', callable: 'callable', parameters: []}
+                return message
+            })
+
+            _remote.receive_(_sender, response(message.id, 'response'))
+
+            await expect(result).resolves.toEqual('response')
+        })
+
+        it('should convert remote response to local', async () => {
+            let message: any
+
+            let result = _remote.send_(_sender, (id) => {
+                message = {id: id, type: 'call', callable: 'callable', parameters: []}
+                return message
+            })
+
+            _remote.receive_(_sender, response(message.id, {_solstice_id: 'func'}))
+
+            await expect(result).resolves.toBeTypeOf('function')
+        })
+
+        it('should throw exception if message id does not match', () => {
+            expect(() => _remote.receive_(_sender, response('unknown'))).toThrowError('callable not called')
+        })
+
+        it('should not return to sender after response received', () => {
+            let message: any
+
+            _remote.send_(_sender, (id) => {
+                message = {id: id, type: 'call', callable: 'callable', parameters: []}
+                return message
+            })
+
+            _remote.receive_(_sender, response(message.id, 'response'))
+            expect(() => _remote.receive_(_sender, response(message.id, 'response'))).toThrowError('callable not called')
+        })
+
+    })
 
     function response(id: string = 'message-id', response: any = 'received'): CallableResponse {
         return {id: id, type: 'response', response: response}
-    }
-
-    function remoteReceived() {
-        return new Promise<any>((resolve) => {
-            _sandbox.contentWindow!.addEventListener('message', (e) => resolve(e.data), {once: true})
-        })
     }
 })
