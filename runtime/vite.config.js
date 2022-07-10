2405404c9cac9dd3fbe55b1f9b54d4462ffc196c
Vincent Xu <vincent.hao.xu@gmail.com>
2022-07-10 22:04:52 +0800

    add rollup ts plugin to generate type declaration

--- runtime/vite.config.js
+++ runtime/vite.config.js
@@ -1,14 +1,16 @@
 import {defineConfig} from 'vite'
 import * as path from 'path'
+import typescript from '@rollup/plugin-typescript'
 
 export default defineConfig({
     build: {
         lib: {
             entry: path.resolve(__dirname, 'src/main.ts'),
-            name:'solstice-runtime',
+            name: 'solstice-runtime',
             fileName: (format) => `solstice-runtime.${format}.js`
         },
         rollupOptions: {
+            plugins: [typescript()]
         }
     }
 })
